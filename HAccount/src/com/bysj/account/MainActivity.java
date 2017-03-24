69ba2b680805cd17858c50be66bf37cbf515948d
chenxin <527437449@qq.com>
2017-03-24 16:47:27 +0800

    增加一个测试页面
    Signed-off-by: chenxin <527437449@qq.com>

--- HAccount/src/com/bysj/account/MainActivity.java
+++ HAccount/src/com/bysj/account/MainActivity.java
@@ -1,9 +1,11 @@
 package com.bysj.account;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
 public class MainActivity extends Activity {
 
@@ -12,23 +14,14 @@ public class MainActivity extends Activity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 	}
-
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.main, menu);
-		return true;
+	
+	//测试SP存储
+	public void onClickSP(View view){
+	
 	}
 
-	@Override
-	public boolean onOptionsItemSelected(MenuItem item) {
-		// Handle action bar item clicks here. The action bar will
-		// automatically handle clicks on the Home/Up button, so long
-		// as you specify a parent activity in AndroidManifest.xml.
-		int id = item.getItemId();
-		if (id == R.id.action_settings) {
-			return true;
-		}
-		return super.onOptionsItemSelected(item);
-	}
+
+	
+	
+	
 }
